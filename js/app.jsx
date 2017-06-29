58d511cf79a10ad6c47d6d089c66cd592e11bfb2
bartekmanu <zrobtodoitnow@gmail.com>
2017-06-29 22:33:38 +0200

    add timer

--- js/app.jsx
+++ js/app.jsx
@@ -4,6 +4,36 @@ require('../sass/style.sass');
 
 document.addEventListener('DOMContentLoaded', function() {
 
+//TIMER
+class YourTime extends React.Component{
+    // constructor(props) {
+        // super(props);
+// 
+        // this.state = {
+            // seconds: 0
+        // };
+    // }
+    // componentDidMount(){
+        // this.intervalId = setInterval(() => {
+            // this.setState({
+                // seconds: this.state.seconds + 1
+            // });
+        // }, 1000);
+    // }
+    // componentWillUnmount(){
+        // clearInterval(this.intervalId);
+    // }
+    render(){
+        return (
+            <div className = 'your_time'>
+                WELCOME
+                {/*<h1>Your time spent for me: {this.state.seconds} s.</h1>*/}
+            </div>
+        )  
+    }
+}
+
+//ICONS
 class AboutMeIcon extends React.Component {
     render() {
         return (
@@ -54,8 +84,18 @@ class ContactIcon extends React.Component {
         );
     }
 }
+class Trash extends React.Component {
+    render() {
+        return (
+            <div className = 'rim'>     
+                <div className = 'trash_icon'></div>
+                <p>Trash</p>                
+            </div>   
+        );
+    }
+}
 
-//ZBIORCZY OBSZAR NA IKONY
+//AREA FOR ICONS
 class BodyForIcons extends React.Component {
     render() {
         return (
@@ -65,6 +105,55 @@ class BodyForIcons extends React.Component {
                 <TechnologiesIcon />
                 <MyWorkIcon />
                 <ContactIcon />
+                <Trash />
+            </div>
+        );
+    }
+}
+
+//AREA FOR WINDOWS
+
+//LOCAL TIME
+// class LocalTime extends React.Component {
+//     render() {
+//         let int = setInterval(()=> {
+//         let hour = new Date().toLocaleTimeString()
+//         },1000)
+//         return (
+//             <div className = 'local_time' >
+//                 <div> <span>Jest godzina:</span> <h1>{ this.hour }</h1> </div>
+//             </div>
+//         );        
+//     }
+// }
+class LocalTime extends React.Component {
+		constructor(props){
+        super(props)
+
+        this.state = {
+        hour: new Date().toLocaleTimeString()
+        }
+    }
+    render() {
+        let int = setInterval(()=> {
+        	this.setState({ hour: new Date().toLocaleTimeString() })
+        },1000)
+        return (
+            <div className = 'local_time' >
+                <div> <h1>{ this.state.hour }</h1> </div>
+            </div>
+        );        
+    }
+}
+
+
+//FOOTER
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className = 'footer' >
+                <LocalTime />     
             </div>
         );
     }
@@ -75,11 +164,14 @@ class Desktop extends React.Component {
     render() {
         return (
             <div className = 'desktop'>
-                <BodyForIcons />
+                <YourTime />,           
+                <BodyForIcons />,
+                <Footer />        
             </div>
         );
     }
 }
+
 //APP WIADOMKA
 class App extends React.Component {
     render() {
