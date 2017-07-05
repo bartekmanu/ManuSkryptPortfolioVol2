ada96e3be58a1969f9dfee3c06415d4b97876786
bartekmanu <zrobtodoitnow@gmail.com>
2017-07-05 12:53:34 +0200

    new file organization

---
+++ js/components/header/header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class YourTime extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            seconds: 0
+        };
+    }
+    componentDidMount(){
+        this.intervalId = setInterval(() => {
+            this.setState({
+                seconds: this.state.seconds + 1
+            });
+        }, 1000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
+    render(){
+        return (
+            <div className = 'your_time'>
+                <p>WELCOME</p>
+                <p>Your time spent for me: {this.state.seconds} s.</p>
+            </div>
+        )  
+    }
+}
